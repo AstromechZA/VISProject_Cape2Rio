9ec5e515a366b981fcabb370efcf1e24865c7793
Ben Meier <benmeier42@gmail.com>
2014-02-17 11:03:30 +0200

    main page template changes, and fake data change

--- py_scripts/fakedatamaker.py
+++ py_scripts/fakedatamaker.py
@@ -2,9 +2,31 @@
 import random
 
 boats = [
-'SCARLET RUNNER',
 'MASERATI',
-'EXPLORA'
+'SCARLET RUNNER',
+'ISKAREEN',
+'PRIVATEER',
+'INVESTEC CIAO BELLA',
+'AMTEC WITS ALADDIN',
+'MUSSULO III',
+'EXPLORA',
+'ROCKET/STOP RHINO POACHING',
+'INSV MHADEI',
+'YOLO',
+'PERIE BANOU ROLLY TASKER',
+'JML ROTARY SCOUT',
+'COOL RUNNINGS',
+'VULCAN 44',
+'JACARANDA OF CARRICK',
+'HOT ICE',
+'MYRTLE OF BONNIEVALE',
+'TULLIANA',
+'NOMAD YACHT CHARTERS',
+'TRANQUILO',
+'WINDGAT',
+'ALLEYCAT TOO',
+'GENEVIEVE TOO',
+'HQ 2'
 ]
 
 days = 28
@@ -15,7 +37,14 @@ for b in boats:
     d[b] = []
 
 for j in range(days):
-    random.shuffle(boats)
+
+    iterations = random.randrange(5)
+    for i in range(iterations):
+        t = random.randrange(len(boats)-1)
+        temp = boats[t]
+        boats[t] = boats[t+1]
+        boats[t+1] = temp
+
     for i in range(len(boats)):
         d[boats[i]] += [i+1, i+1, i+1]
 
